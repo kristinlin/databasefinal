78213562ddb21e290476cef5aa727f450c92930b
kristinlin <lin.kr@husky.neu.edu>
2021-12-04 15:24:43 -0500

    finish all fe for pages for now

--- app.py
+++ app.py
@@ -1,47 +1,54 @@
 from flask import Flask, session, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.secret_key = 'REALLYSECRET'
+app.secret_key = "REALLYSECRET"
 
 
 @app.route("/", methods=["GET"])
 def homepage():
-    if 'user' in session: 
-        print(session['user'])
+    if "user" in session:
+        print(session["user"])
         return render_template("homepage.html")
-    else: 
+    else:
         return redirect(url_for("login"))
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     fail = False
-    if request.method == 'POST':
-        nuid = request.form['nuid']
-        pwd = request.form['pwd']
+    if request.method == "POST":
+        nuid = request.form["nuid"]
+        pwd = request.form["pwd"]
         print(nuid + " " + pwd)
-        if nuid == "00000": # TODO check user exists
-            session['user'] = nuid
+        if nuid == "00000":  # TODO check user exists
+            session["user"] = nuid
             return redirect(url_for("homepage"))
         fail = True
-    return render_template("login.html", fail = fail)
+    return render_template("login.html", fail=fail)
+
 
 @app.route("/account/logout", methods=["GET"])
 def logout():
-    session.pop('user')
+    session.pop("user")
     return redirect(url_for("login"))
+
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
     if "user" not in session:
         return redirect(url_for("login"))
     changePwd = False
-    if request.method == 'POST':
-        pwd = request.form['pwd']
+    if request.method == "POST":
+        pwd = request.form["pwd"]
         print("new password: " + pwd)
         # TODO actually change password
         changePwd = True
-    return render_template("account.html", student_name="Tester Name", nuid=session['user'], changePwd=changePwd)
+    return render_template(
+        "account.html",
+        student_name="Tester Name",
+        nuid=session["user"],
+        changePwd=changePwd,
+    )
 
 
 @app.route("/search", methods=["GET", "POST"])
@@ -49,28 +56,68 @@ def search():
     if "user" not in session:
         return redirect(url_for("login"))
     results = []
-    if request.method == 'POST':
+    if request.method == "POST":
         # TODO actually find the results
-        results = [{"id":"123", "col1":"hello", "col2":"hello2", "col3":"hello3"},
-        {"id": "123", "col1": "hello", "col2": "hello2", "col3": "hello3"}]
-    return render_template("search.html", courseNum=["tester1", "tsester2"], results=results)
+        results = [
+            {"id": "123", "col1": "hello", "col2": "hello2", "col3": "hello3"},
+            {"id": "123", "col1": "hello", "col2": "hello2", "col3": "hello3"},
+        ]
+    return render_template(
+        "search.html", courseNum=["tester1", "tsester2"], results=results
+    )
 
 
 @app.route("/courses", methods=["GET"])
 def courses():
     if "user" not in session:
         return redirect(url_for("login"))
-    courses = [{"subject":"CS", "num":1800, "semester": "Fall2020", "title":"Discrete Structures", "reviewed":True},
-    {"subject": "CS", "num": 3200, "semester": "Fall2020", "title": "Database Design", "reviewed": False}]
+    courses = [
+        {
+            "subject": "CS",
+            "num": 1800,
+            "semester": "Fall2020",
+            "title": "Discrete Structures",
+            "reviewed": True,
+        },
+        {
+            "subject": "CS",
+            "num": 3200,
+            "semester": "Fall2020",
+            "title": "Database Design",
+            "reviewed": False,
+        },
+    ]
     return render_template("courses.html", courses=courses)
+
 
 @app.route("/course", methods=["GET"])
 def course():
-    return render_template("course.html", courses=courses)
+    semester = "All semesters"
+    like = False
+    courseId = request.args.get("courseId")
+    if "semester" in request.args:
+        semester = request.args.get("semester")
+    if "like" in request.args:
+        like = request.args.get("like") == "True"
+    reviews = [
+        {"id": 123, "comment": "Wooohoo my comment is here", "rating": 9.6, "helpful": 6}
+    ]
+    return render_template(
+        "course.html",
+        professor="Kathleen Durane",
+        courseId=courseId,
+        course="CS3200 Database Design",
+        semester=semester,
+        reviews=reviews,
+        like = like
+    )
+
 
 @app.route("/write", methods=["GET"])
-def write():    
-    return render_template("write.html", professor="Kathleen Durane", course="CS3200 Database Design")
+def write():
+    return render_template(
+        "write.html", professor="Kathleen Durane", course="CS3200 Database Design"
+    )
 
 
 @app.route("/write/review", methods=["POST"])
